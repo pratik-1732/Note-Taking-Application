b357ca95e847f131544161a2860dca11875e5869
Pratik <patilpratikp035@gmail.com>
2025-07-03 19:56:58 +0530

    project initialized and creating signup page

---
+++ client/components/Signup.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Signup = () => {
+  return <div>
+    <div className="left-col">
+
+    </div>
+    <div className="right-col">
+        <img className="p-32 rounded-[24px]"/>
+    </div>
+  </div>;
+};
+
+export default Signup;
