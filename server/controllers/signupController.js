bf20052b8186da503fd941b457810651d2495b54
Pratik <patilpratikp035@gmail.com>
2025-07-05 20:45:36 +0530

    solved the problem of id requesting to fetch details

--- server/controllers/signupController.js
+++ server/controllers/signupController.js
@@ -1,5 +1,6 @@
 import express from "express";
 import User from "../models/user.js";
+import jwt from "jsonwebtoken";
 
 const createToken = (payload) => {
   return jwt.sign(payload, process.env.JWT_SECRET, {
@@ -12,7 +13,7 @@ const signupUser = async (req, res) => {
 
   const newUser = await User.findOne({ email });
   if (!newUser) {
-    await User.create({ name, email, dob });
+    const createdUser = await User.create({ name, email, dob });
     const token = createToken({ email });
     return res
       .cookie("token", token, {
@@ -20,7 +21,7 @@ const signupUser = async (req, res) => {
         maxAge: 24 * 60 * 60 * 1000,
       })
       .status(200)
-      .json({ message: "user created successfully", userId: User._id });
+      .json({ message: "user created successfully", userId: createdUser._id });
   } else {
     return res.status(400).json({ message: "user already registered" });
   }
